c6fbc5b72b9e3c64accf5948a0c9453567315e47
Matsadura <zidanditoo1@gmail.com>
2023-08-15 23:15:01 +0100

    updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
-void print_to_98(int n);
+void times_table(void);
+void print_times_table(int n);
+void comma_3spaces(void);
