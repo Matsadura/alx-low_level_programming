cab73060ceb5dfe650869203f8ef4b7bd7b53695
Matsadura <zidanditoo1@gmail.com>
2023-08-17 21:44:10 +0100

    added task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,9 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num <= 9)
-				_putchar(num + '0');
-			else
-				_putchar(num + '0');
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
