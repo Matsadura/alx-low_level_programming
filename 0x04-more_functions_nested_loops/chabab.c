9ab3f2d51734b8aaced464842eebdd58e1276dd0
Matsadura <zidanditoo1@gmail.com>
2023-08-19 15:32:35 +0100

    removed vims

---
+++ 0x04-more_functions_nested_loops/chabab.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+                 
+int main(void)
+{
+	for (i reazr fdsqfzeaf dsq;)
+		if (fdsq)
+		{
+			fdsqfsdqfqs;
+		}
+
+	printf("No more whitespace \n");
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/chchch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void print_shape(int num1, int num2);
+
+void print_shape(int num1, int num2)
+{
+	int idx1, idx2;
+
+	for (idx1 = 0; idx1 < num1; idx1++)
+	{
+		for (idx2 = 0; idx2 < num2; idx2++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
+/**
+ * main - calls print_shape()
+ *
+ * Return: Always 0.
+ **/
+int main(void)
+{
+	print_shape(4, 3);
+	return (0);
+}
