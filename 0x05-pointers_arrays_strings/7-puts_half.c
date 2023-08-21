c93e247c248500e2259c397678bdd2b54f266deb
Matsadura <zidanditoo1@gmail.com>
2023-08-21 18:27:28 +0100

    Updated odd case in task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -26,7 +26,7 @@ void puts_half(char *str)
 {
 	int i;
 	int n1 = _strlen(str) / 2;
-	int n2 = (_strlen(str)) / 2;
+	int n2 = (_strlen(str) - 1) / 2;
 
 	if (_strlen(str) % 2 == 0)
 	{
@@ -37,7 +37,7 @@ void puts_half(char *str)
 	}
 	else if (_strlen(str) % 2 != 0)
 	{
-		for (i = n2; str[i] != '\0'; i++)
+		for (i = n2 + 1; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
