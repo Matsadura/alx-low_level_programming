3a88e934a4dd17defe3ff58b3d2d0806e4f492f1
Matsadura <zidanditoo1@gmail.com>
2023-08-21 18:23:23 +0100

    Added task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int n1 = _strlen(str) / 2;
+	int n2 = (_strlen(str) - 1) / 2;
+
+	if (_strlen(str) % 2 == 0)
+	{
+		for (i = n1; str[i] != '\0' ; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (_strlen(str) % 2 != 0)
+	{
+		for (i = n2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
