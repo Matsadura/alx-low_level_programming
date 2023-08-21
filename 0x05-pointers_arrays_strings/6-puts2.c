99d9379ded8c0e2034a818334dbf8e9fe1d5e9e1
Matsadura <zidanditoo1@gmail.com>
2023-08-21 17:35:07 +0100

    Added task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of
+ *	a string
+ * @str: the string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
