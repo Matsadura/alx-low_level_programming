76f707a707b40ba7f8d8b8ca3d7fa24631b24c82
Matsadura <zidanditoo1@gmail.com>
2023-08-21 16:37:15 +0100

    Added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
