0748c89d38e764bc984e42f17b3d41ba6903e603
Matsadura <zidanditoo1@gmail.com>
2023-08-24 11:57:54 +0100

    Added task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: the string to encode
+ * Return: pointer to the string
+ */
+
+char *leet(char *s)
+{
+	char alpha[] = "aeotl";
+	char num[] = "43071";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0' ; j++)
+		{
+			if (s[i] == alpha[j] || s[i] == alpha[j] - 32)
+				s[i] = num[j];
+		}
+	}
+	return (s);
+}
