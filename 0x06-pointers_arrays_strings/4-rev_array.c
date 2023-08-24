cb92f92fa6bd85adf7b10f364a8b73a81d41961c
Matsadura <zidanditoo1@gmail.com>
2023-08-24 01:28:10 +0100

    Added task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an
+ *	array of integers
+ * @a: an array of integers
+ * @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+	int j1 = n / 2, j2 = (n - 1) / 2;
+
+	if (n % 2 == 0)
+	{
+		for (i = 0 ; i <= j1; i++)
+		{
+			tmp = *(a + i);
+			*(a + i) = *(a + n - 1 - i);
+			*(a + n - 1 - i) = tmp;
+		}
+	}
+	else if (n % 2 != 0)
+	{
+		for (i = 0 ; i <= j2; i++)
+		{
+			tmp = *(a + i);
+			*(a + i) = *(a + n - 1 - i);
+			*(a + n - 1 - i) = tmp;
+
+			if (i == j2)
+			{
+				continue;
+			}
+		}
+	}
+
+}
