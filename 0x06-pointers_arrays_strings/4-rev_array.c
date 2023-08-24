382e9e66c941b4da1ecce1380b410bfaa16b790f
Matsadura <zidanditoo1@gmail.com>
2023-08-24 04:21:58 +0100

    Change the whole code of task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,37 +1,22 @@
 #include "main.h"
+
 /**
  * reverse_array - reverses the content of an
  *	array of integers
- * @a: an array of integers
- * @n: number of elements
+ * @a: the array
+ * @n: the number of elements of the array
  */
 
 void reverse_array(int *a, int n)
 {
-	int i, tmp;
-	int j1 = n / 2, j2 = (n - 1) / 2;
+	int i, tmp, j = n - 1;
 
-	if (n == 0)
-		a[0] = 0;
-
-	if (n % 2 == 0)
+	for (i = 0; i < j ; i++)
 	{
-		for (i = 0 ; i <= j1; i++)
-		{
-			tmp = *(a + i);
-			*(a + i) = *(a + n - 1 - i);
-			*(a + n - 1 - i) = tmp;
-		}
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		j--;
 	}
-	else
-	{
-		for (i = 0 ; i < j2; i++)
-		{
-			tmp = *(a + i);
-			*(a + i) = *(a + n - 1 - i);
-			*(a + n - 1 - i) = tmp;
-
-		}
-	}
-
 }
+
