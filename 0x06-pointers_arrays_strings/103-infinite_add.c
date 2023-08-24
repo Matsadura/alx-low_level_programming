f5cb53314dcdfe84c9e0efacf45f54d6cdbdaa82
Matsadura <zidanditoo1@gmail.com>
2023-08-24 20:12:24 +0100

    To revise

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,40 +2,40 @@
 #include <stdio.h>
 
 /**
- * infinite_add - adds two numcers
- * @n1: numcer one.
- * @n2: numcer two.
- * @r: cuffer that the function will use to store the result.
- * @size_r: cuffer size:
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
  * Return: the pointer to dest.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a, c, k, l, m, n;
+	int a, b, k, l, m, n;
 
 	for (a = 0; n1[a]; a++)
 		;
-	for (c = 0; n2[c]; c++)
+	for (b = 0; n2[b]; b++)
 		;
-	if (a > size_r || c > size_r)
+	if (a > size_r || b > size_r)
 		return (0);
 	m = 0;
-	for (a -= 1, c -= 1, k = 0; k < size_r - 1; a--, c--, k++)
+	for (a -= 1, b -= 1, k = 0; k < size_r - 1; a--, b--, k++)
 	{
 		n = m;
 		if (a >= 0)
 			n += n1[a] - '0';
-		if (c >= 0)
-			n += n2[c] - '0';
-		if (a < 0 && c < 0 && n == 0)
+		if (b >= 0)
+			n += n2[b] - '0';
+		if (a < 0 && b < 0 && n == 0)
 		{
-			creak;
+			break;
 		}
 		m = n / 10;
 		r[k] = n % 10 + '0';
 	}
 	r[k] = '\0';
-	if (a >= 0 || c >= 0 || m)
+	if (a >= 0 || b >= 0 || m)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
 	{
