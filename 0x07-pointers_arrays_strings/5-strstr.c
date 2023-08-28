f1f7f003415ad4355b4b30ed8e7965cae404c555
Matsadura <zidanditoo1@gmail.com>
2023-08-28 20:03:20 +0100

    Added task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * len - return the length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+unsigned int len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string
+ * @needle: the substring
+ * Return: a pointer to the beginning of the
+ *	located substring or NULL if the
+ *	substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (needle[j] == haystack[i + j])
+				return (haystack + i);
+			else
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i -len(needle));
+	}
+	if (haystack[i] == needle[j])
+		return (haystack + i - len(needle));
+	return (NULL);
+}
