472b7a3c55848f0580fcc95525c947bf4184b2d0
Matsadura <zidanditoo1@gmail.com>
2023-08-28 17:09:15 +0100

    try 6, task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,12 +14,12 @@ char *_strchr(char *s, char c)
 	int i = 0;
 
 	if (c == '\0')
-		return (s + i);
+		return (&s[i]);
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (s + i);
+			return (&s[i]);
 		}
 	}
 	return (NULL);
