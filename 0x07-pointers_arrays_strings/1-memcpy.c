4655f8f95b3726317a2d1f9849b5369489a48c8a
Matsadura <zidanditoo1@gmail.com>
2023-08-28 15:18:51 +0100

    Added task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area
+ *	src to memory area dest
+ * @src: source memory area
+ * @dest: destination memory area
+ * @n: the n bytes to copy
+ * Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
