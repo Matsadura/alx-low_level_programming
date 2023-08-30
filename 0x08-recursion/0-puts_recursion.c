2b9fd4eac99f75dd6a2e7fe261ed1c1fa15fb642
Matsadura <zidanditoo1@gmail.com>
2023-08-30 13:08:36 +0100

    Add task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the string
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
+}
