ee91d7da8c8e19e2ec3bb29029084ceec04c2008
Matsadura <zidanditoo1@gmail.com>
2023-09-01 12:38:08 +0100

    Add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program's name
+ * @argc: Argument counts
+ * @argv: Arguments
+ * Return: First string
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
