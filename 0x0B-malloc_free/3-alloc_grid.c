88c1644d8eedabb8b3d82d45b2fc20ad7d8aa673
Matsadura <zidanditoo1@gmail.com>
2023-09-05 14:19:10 +0100

    Try 2 malloc return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,10 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			free(arr);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
