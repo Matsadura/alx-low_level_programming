e49e21d053003ad2aa31d92b759d9566e296f37d
Matsadura <zidanditoo1@gmail.com>
2023-09-05 14:08:38 +0100

    Fix betty and MAYBE the amount of memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,14 +14,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int**)malloc(sizeof(int*) * height);
+	arr = (int **)malloc(sizeof(int *) * height);
 
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int*) * width);
+		arr[i] = malloc(sizeof(int) * width);
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
