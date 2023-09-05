f2d70476a22bfaa55675650ef3a8aa25c346c4f7
Matsadura <zidanditoo1@gmail.com>
2023-09-05 13:08:22 +0100

    Add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * str_concat - Concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Pointer to a newly allocated space
+ *	in memory which contains the contents
+ *	of s1 followed by s2 and null terminated
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j = 0, len1, len2;
+	char *str;
+
+	if (s1 == NULL)
+		len1 = 1;
+	else
+		len1 = _strlen(s1);
+	if (s2 == NULL)
+		len2 = 1;
+	else
+		len2 = _strlen(s2);
+
+	str = malloc(len1 + len2);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1 ; i++)
+	{
+		str[j] = s1[i];
+		j++;
+	}
+	for (i = 0; i < len2 ; i++)
+	{
+		str[j] = s2[i];
+		j++;
+	}
+
+	str[j] = '\0';
+	return (str);
+}
