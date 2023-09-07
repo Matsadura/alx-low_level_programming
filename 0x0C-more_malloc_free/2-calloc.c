13b40be7737935744eddcb0abcec19b6fb7e0ada
Matsadura <zidanditoo1@gmail.com>
2023-09-07 14:07:48 +0100

    set memory to 0 with _memset

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: the first bytes of the memory area
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
+
+/**
  * _calloc - Allocates memory for an array
  * @nmemb: Number of elements
  * @size: size of each element
@@ -14,9 +33,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * size);
+	arr = (void *)malloc(nmemb * size);
+
 	if (arr == NULL)
 		return (NULL);
+	_memset(arr, 0x00, nmemb);
 
 	return (arr);
 }
