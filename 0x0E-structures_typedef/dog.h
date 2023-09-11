85ccef77a2e6f0995d29f667a9abbfc2287251c2
Matsadura <zidanditoo1@gmail.com>
2023-09-11 19:33:12 +0100

    Add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - poppy structure
+ * @name: poppy's name
+ * @age: poppy's age
+ * @owner: poppy's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
