7d888b1e9d79f0ce7ee2e4f29d2cbc2ba59eaf7b
Matsadura <zidanditoo1@gmail.com>
2023-09-11 20:11:22 +0100

    Add task 1, update dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type
+ *	struct dog
+ * @d: pointer to struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
