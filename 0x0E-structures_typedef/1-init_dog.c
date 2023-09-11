45d3a60aeaebf85637e21e92f701e2cf8464b4ab
Matsadura <zidanditoo1@gmail.com>
2023-09-11 20:22:55 +0100

    Fix null case task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initializes a variable of type
@@ -11,6 +12,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
