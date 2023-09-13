353e62f6985ebddfc365d759b0f949ab1a3f87b6
Matsadura <zidanditoo1@gmail.com>
2023-09-13 19:02:18 +0100

    try 3 task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i = 0;
 
-	if (size == 0 || array == NULL)
+	if (action == NULL || size == 0 || array == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
