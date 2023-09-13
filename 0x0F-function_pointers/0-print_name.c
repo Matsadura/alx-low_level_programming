06eb9778538493d17902508cc0e702cd7066d69b
Matsadura <zidanditoo1@gmail.com>
2023-09-13 15:54:55 +0100

    Try 4 task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,21 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * _puts - prints a string
- * @s: the string
- */
-void _puts(char *s)
-{
-	int i;
-
-	if (s == NULL)
-		return;
-	for (i = 0; s[i] != '\0'; i++)
-		_putchar(s[i]);
-	_putchar('\n');
-}
-
-/**
  * print_name - prints a name
  * @name: the name
  * @f: fucntion pointer
