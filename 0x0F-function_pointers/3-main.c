ec837f46c3d2d7f9118a777ed5a1a867d77bcf36
Matsadura <zidanditoo1@gmail.com>
2023-09-13 22:35:30 +0100

    Add task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b;
+	char *op;
+
+	op = argv[2];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (strcmp(op, "/") != 0 && strcmp(op, "*") != 0
+			&& strcmp(op, "+") != 0 && strcmp(op, "%") != 0 && strcmp(op, "-") != 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+
+	if ((strcmp(argv[2], "/") == 0 && b == 0)
+			|| (strcmp(argv[2], "%") == 0 && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculate the modulos
+ * @a: first number
+ * @b: second number
+ * Return: the result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
