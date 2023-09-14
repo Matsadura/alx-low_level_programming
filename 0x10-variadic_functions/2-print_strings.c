fa95edc4145035ae8b3630f6e2cdcdd13d0dd614
Matsadura <zidanditoo1@gmail.com>
2023-09-14 18:30:38 +0100

    Removed changes

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,15 +14,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(str, n);
 
-	for (i = 0; i < n; i++)
-	{
-		if (va_arg(str, char *) == NULL)
-		{
-			printf("(nil)\n");
-			return;
-		}
-
-	}
 
 	for (i = 0; i < n; i++)
 	{
