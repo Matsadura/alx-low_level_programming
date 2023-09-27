0ab7aedb0f5c8b99048611516de2a61f963c0551
Matsadura <zidanditoo1@gmail.com>
2023-09-27 01:03:14 +0100

    Fix index typo

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	current = *head;
-	if (i == 0)
+	if (index == 0)
 	{
 		*head = current->next;
 		free(current);
