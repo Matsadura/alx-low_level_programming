fcb2af0453ac8bdb9b7b65ed3dbd364da2710a74
Matsadura <zidanditoo1@gmail.com>
2023-09-28 15:00:04 +0100

    Add task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representa-
+ *	tion of a number
+ * @n: the number
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int tmp = n;
+	int i = 0;
+
+	if (n == 0)
+		_putchar(0 + '0');
+
+	while (tmp != 0)
+	{
+		tmp >>= 1;
+		i++;
+	}
+	i--;
+
+	while (i >= 0)
+	{
+		tmp = n;
+		tmp >>= i;
+		_putchar(tmp + '0');
+		tmp <<= i;
+		n = n - tmp;
+		i--;
+	}
+}
