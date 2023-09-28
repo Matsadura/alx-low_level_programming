ef02f2c33b22a3f058dc578fa93bfd645622552d
Matsadura <zidanditoo1@gmail.com>
2023-09-28 15:14:56 +0100

    Fix tmp type

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,28 +8,24 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int tmp = n;
+	unsigned long int tmp = n;
 	int i = 0;
 
 	if (n == 0)
-		_putchar('0');
+		_putchar(0 + '0');
 
-	while (tmp >= 1)
+	while (tmp != 0)
 	{
 		tmp >>= 1;
 		i++;
 	}
-	tmp = n;
 	i--;
 
 	while (i >= 0)
 	{
 		tmp = n;
 		tmp >>= i;
-		if (tmp == 1)
-			_putchar(1 + '0');
-		else
-			_putchar(0 + '0');
+		_putchar(tmp + '0');
 		tmp <<= i;
 		n = n - tmp;
 		i--;
