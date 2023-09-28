21500a2a65f335a711fce8e129db7ce29537d8ba
Matsadura <zidanditoo1@gmail.com>
2023-09-28 17:18:05 +0100

    Add error case

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,15 +10,21 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int m = 1;
-	unsigned int b;
+	unsigned int i;
+	unsigned long int m = 1;
+	unsigned long int b, tmp = n;
 
-	if (n == 0)
-		return (0);
+	while (n != 0)
+	{
+		n >>= 1;
+		i++;
+		if (index > i)
+			return (-1);
+	}
 
 	m <<= index;
 
-	b = (n & m) >> index;
+	b = (tmp & m) >> index;
 
 	return (b);
 }
