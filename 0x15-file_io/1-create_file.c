34d18dfc548895a64492e6741f4418e63f5ca2f9
Matsadura <zidanditoo1@gmail.com>
2023-10-02 20:21:41 +0100

    Change text = NULL condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,14 +27,15 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	w = write(fd, text_content, _strlen(text_content));
+	if (text_content != NULL)
+		w = write(fd, text_content, _strlen(text_content));
 	if (w == -1)
 		return (-1);
 
