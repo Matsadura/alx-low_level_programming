83254a1d5f3ccb7005c39d514b5c29038f57bb84
Matsadura <zidanditoo1@gmail.com>
2023-10-02 20:19:27 +0100

    Change RDWR to WRONLY

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
