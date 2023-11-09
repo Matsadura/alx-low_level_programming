39e9cbeb0c4fe9bdbe8dfcdc135671e59dc7dd46
Matsadura <zidanditoo1@gmail.com>
2023-11-09 21:25:53 +0100

    add task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node
+ *	of a doubly linked list
+ * @head: the head of the list
+ * @index: the index of the node
+ * Return: the node at the index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int len = 0;
+	dlistint_t *current = head;
+
+	while (len < index)
+	{
+		current = current->next;
+		len++;
+	}
+
+	return (current);
+}
