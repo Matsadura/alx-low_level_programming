81a92988808c36fd13fbcaa97be7c863c4a81fb4
Matsadura <zidanditoo1@gmail.com>
2023-11-09 23:46:36 +0100

    add free

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -46,5 +46,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		(current->prev)->next = current->next;
 		(current->next)->prev = current->prev;
 	}
+	free(current);
 	return (1);
 }
