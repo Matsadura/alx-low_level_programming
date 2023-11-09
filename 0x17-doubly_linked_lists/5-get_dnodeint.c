b16e30f70545e6a80b6c96b7a969985934ee8d39
Matsadura <zidanditoo1@gmail.com>
2023-11-09 21:28:36 +0100

    Add null case

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,8 +13,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int len = 0;
 	dlistint_t *current = head;
 
+
+
 	while (len < index)
 	{
+		if (current == NULL)
+			return (NULL);
 		current = current->next;
 		len++;
 	}
