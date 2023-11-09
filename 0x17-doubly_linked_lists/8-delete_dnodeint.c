0080e5a1f0b70b5ed7344a1a292ee9a63fcce0bf
Matsadura <zidanditoo1@gmail.com>
2023-11-09 23:37:24 +0100

    add task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a note at
+ *	and index in a doubly linked list
+ * @head: the head of the list
+ * @index: the index of the node
+ * Return: 1 if it succeeded, -1 otherwise
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head, *length = *head;
+	unsigned int len = 0, count = 0;
+
+
+	while (length)
+	{
+		length = length->next;
+		len++;
+	}
+
+	if (*head == NULL || index > len)
+		return (-1);
+
+	while (count < index)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (index == 0)
+	{
+		current = *head;
+		*head = current->next;
+		if (current->next)
+		{
+			if (len != 0)
+				(current->next)->prev = *head;
+			else
+				current->prev = NULL;
+		}
+	}
+	else
+	{
+		(current->prev)->next = current->next;
+		(current->next)->prev = current->prev;
+	}
+	return (1);
+}
