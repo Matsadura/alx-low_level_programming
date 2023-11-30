60e59df657b971f6ce5f6afd00edefe7953f8b48
Matsadura <zidanditoo1@gmail.com>
2023-11-30 16:43:37 +0100

    Add array = null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,6 +16,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
