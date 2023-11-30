e44b300d7d97b43b22054d09ada916dad66200cb
Matsadura <zidanditoo1@gmail.com>
2023-11-30 16:57:52 +0100

    Add cast

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
 
