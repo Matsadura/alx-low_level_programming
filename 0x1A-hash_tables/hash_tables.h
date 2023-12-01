e462705519f742b676845a5450fecc5ac60d8d13
Matsadura <zidanditoo1@gmail.com>
2023-12-01 22:54:08 +0100

    update header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,10 @@
-#ifndef HASH_TABLES
-#define HASH_TABLES
-#include <stdio.h>
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
+
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -33,6 +35,12 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 /**
  * struct shash_node_s - Node of a sorted hash table
@@ -71,11 +79,11 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-int _putchar(char c);
-#endif /* HASH_TABLES */
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
+#endif /* _HASH_TABLES_H_ */
