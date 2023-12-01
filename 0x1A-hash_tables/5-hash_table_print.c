b58bc7cd949db2110e5003216f867c782ddb5a48
Matsadura <zidanditoo1@gmail.com>
2023-12-01 14:42:38 +0100

    Add array is null condition

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,26 +11,23 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *current;
 	int flag = 0;
 
-	if (ht == NULL)
-		printf("{}\n");
+	if (ht == NULL || ht->array == NULL)
+		return;
 
-	else
+	printf("{");
+	for (i = 0; i < ht->size; i++)
 	{
-		printf("{");
-		for (i = 0; i < ht->size; i++)
+		current = ht->array[i];
+		while (current != NULL)
 		{
-			current = ht->array[i];
-			while (current != NULL)
+			if (flag == 1)
 			{
-				if (flag == 1)
-				{
-					printf(", ");
-				}
-				printf("'%s': '%s'", current->key, current->value);
-				flag = 1;
-				current = current->next;
+				printf(", ");
 			}
+			printf("'%s': '%s'", current->key, current->value);
+			flag = 1;
+			current = current->next;
 		}
-		printf("}\n");
 	}
+	printf("}\n");
 }
