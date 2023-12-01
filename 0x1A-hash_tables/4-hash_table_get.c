df2479df1a5c5d325dc300f61e53abda19acb2f2
Matsadura <zidanditoo1@gmail.com>
2023-12-01 14:02:42 +0100

    Add key is null or empty case

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current;
 	unsigned long int index = key_index((const unsigned char *) key, ht->size);
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (NULL);
 	if (ht->array[index] != NULL)
 	{
