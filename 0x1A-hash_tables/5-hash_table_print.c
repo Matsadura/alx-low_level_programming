984f929b8fba3119c3e31e06765f1833010d12bf
Matsadura <zidanditoo1@gmail.com>
2023-12-01 14:40:57 +0100

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int flag = 0;
+
+	if (ht == NULL)
+		printf("{}\n");
+
+	else
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				if (flag == 1)
+				{
+					printf(", ");
+				}
+				printf("'%s': '%s'", current->key, current->value);
+				flag = 1;
+				current = current->next;
+			}
+		}
+		printf("}\n");
+	}
+}
