df28b51de59223111511475c5cac7cadc6b45631
Matsadura <zidanditoo1@gmail.com>
2023-12-01 13:57:36 +0100

    Fix wrong variable

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,8 +18,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		current = ht->array[index];
 		while (current != NULL)
 		{
-			if (strcmp(ht->array[index]->key, key) == 0)
-				return (ht->array[index]->value);
+			if (strcmp(current->key, key) == 0)
+				return (current->value);
 			current = current->next;
 		}
 	}
